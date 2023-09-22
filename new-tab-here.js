9726a2c5a137dba61b91f9c3c0d87cab2be420ab
Joseph Diza <josephm.diza@gmail.com>
2023-09-21 18:24:26 -0700

    Fix open-tab-before not working

--- new-tab-here.js
+++ new-tab-here.js
@@ -1,11 +1,24 @@
 function open_new_tab(at) {
-  let active_tab = tabs.getCurrent();
-  let here = active_tab.index + at;
-  let new_tab_settings = {
-    active: true,
-    index: here,
-  };
-  tabs.create(new_tab_settings);
+  //let active_tab = browser.tabs.getCurrent();
+  let active_tab = browser.tabs.query({active: true, currentWindow: true});
+  console.log(active_tab);
+  active_tab.then(
+  (active_tab) => {
+    var active_tab = active_tab[0];
+    if (active_tab == null) {
+      console.log("Active tab is null");
+      //console.log(active_tab);
+      return;
+    }
+    let here = active_tab.index + at;
+    let new_tab_settings = {
+      active: true,
+      index: here,
+    };
+
+    browser.tabs.create(new_tab_settings);
+  },
+  (error) => { console.log(error); });
 }
 
 // Open new tab after the current active tab
@@ -22,7 +35,8 @@ function open_before() {
 // Main
 //
 
-function load_extension() {
+//function load_extension() {
+function new_tab_listener() {
   // Create the shortcut listener
   document.addEventListener('keydown', (event) => {
     if (event.shiftKey) {
@@ -32,4 +46,17 @@ function load_extension() {
   }, false);
 }
 
-document.addEventListener("DOMContentLoaded", load_extension);
+//document.addEventListener("DOMContentLoaded", load_extension);
+
+//browser.commands.onCommand.addListener(open_before, "open-tab-before");
+//browser.commands.onCommand.addListener(open_after, "open-tab-after");
+
+browser.commands.onCommand.addListener((command) => {
+  if (command == 'open-tab-before') { console.log(command); open_before(); }
+});
+browser.commands.onCommand.addListener((command) => {
+  if (command == 'open-tab-after') { console.log(command); open_after(); }
+});
+
+//browser.commands.onCommand.addListener(new_tab_listener, "open-tab-before");
+//browser.commands.onCommand.addListener(new_tab_listener, "open-tab-after");
